f8eba3c32571b36a5454159989cbbbf1db8e2e8e
d95-waka <D950@protonmail.com>
2023-08-23 16:50:49 +0300

    Fixed what I broke last commit, and added wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+config = wezterm.config_builder()
+
+-- Behevior
+config.bidi_enabled = true
+config.bidi_direction = 'LeftToRight'
+
+-- Appearance
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0
+}
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.8
+}
+config.window_background_opacity = 0.95
+config.font = wezterm.font_with_fallback{
+	'monospace',
+	'Liberation Mono'
+}
+config.use_fancy_tab_bar = false
+
+-- Keybindings
+config.leader = { key = 'a', mods = 'CTRL|SHIFT', timeout_milliseconds = 1000 }
+
+return config
